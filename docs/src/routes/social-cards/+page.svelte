a03503e3174355f62b7b39e5901ffc7ac04a0a8d
Jason Kummerl <jason.h@kummerl.com>
2025-01-05 11:57:07 -0500

    Do not allow downloading

--- docs/src/routes/social-cards/+page.svelte
+++ docs/src/routes/social-cards/+page.svelte
@@ -165,7 +165,7 @@
     >
         Twitter
     </a>
-    <button
+    <!-- <button
         onclick={downloadCard}
         class="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-md transition-colors flex items-center gap-2"
     >
@@ -173,7 +173,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
         </svg>
         Download {cardType.toUpperCase()}
-    </button>
+    </button> -->
 </div>
 
 <style>
