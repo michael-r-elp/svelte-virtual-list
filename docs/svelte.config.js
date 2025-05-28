e554e2dd1f816e4ff40b63f1c9aca1bc14f1240e
Jason Kummerl <jason.h@kummerl.com>
2025-05-28 15:42:25 -0400

    Current state

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-cloudflare'
+import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
-    preprocess: [vitePreprocess()],
+    preprocess: [vitePreprocess(), mdsvex()],
 
     kit: {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
