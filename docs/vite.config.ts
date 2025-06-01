015ef6281530c6aee9b02eedd4b2f55739fdffe3
Jason Kummerl <jason.h@kummerl.com>
2025-06-01 14:22:42 -0400

    General cleanup and updating

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,7 @@
 import { sentrySvelteKit } from '@sentry/sveltekit'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     plugins: [
