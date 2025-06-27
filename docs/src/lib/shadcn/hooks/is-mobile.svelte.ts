c816e9422458201a6786c3b2fe07b311a44797e0
Jason Kummerl <jason.h@kummerl.com>
2025-06-26 20:18:42 -0400

    Doc's updates

--- docs/src/lib/shadcn/hooks/is-mobile.svelte.ts
+++ docs/src/lib/shadcn/hooks/is-mobile.svelte.ts
@@ -3,6 +3,7 @@ import { MediaQuery } from 'svelte/reactivity'
 const DEFAULT_MOBILE_BREAKPOINT = 768
 
 export class IsMobile extends MediaQuery {
+    /* trunk-ignore(eslint) */
     constructor(breakpoint: number = DEFAULT_MOBILE_BREAKPOINT) {
         super(`max-width: ${breakpoint - 1}px`)
     }
